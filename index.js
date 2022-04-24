e1b9affe03abf4b878a353bf1fd7467dfa948602
ishanbagchi <ishanbagchi4@gmail.com>
2022-04-24 13:46:54 +0530

    add process.env.PORT

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 const app = express()
 
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 const sources = [
 	{
 		name: 'space',
